ecbb6db5a62c562520f20757665baf2b2a996725
Vitor <vitorwerle78@gmail.com>
2024-12-29 15:05:49 -0300

    finish

--- students.py
+++ students.py
@@ -1,12 +1,8 @@
-students = []
+import csv
 
-with open("students.csv") as file:
-    for line in file:
-        name, house = line.rstrip().split(",")
-        student = {}
-        student["name"] = name
-        student["house"] = house
-        students.append(student)
-    
-for student in students:
-    print(f"{student['name']} is in {student['house']}")
+name = input("What's your name? ")
+home = input("Where's your home? ")
+
+with open("estudantes.csv", "a") as file:
+    writer = csv.DictWriter(file, fieldnames=["name", "home"])
+    writer.writerow({"name": name, "home": home})
