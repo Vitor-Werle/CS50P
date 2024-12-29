35b0c29eefcab53eef5dcaa47d90eb19b910f7fc
Vitor <vitorwerle78@gmail.com>
2024-12-29 12:29:31 -0300

    new

--- name.py
+++ name.py
@@ -1,4 +1,8 @@
-name = input("What's your name? ")
+names = []
 
-with open("names.txt", "a") as file:
-    file.write(f"{name}\n")
+with open("names.txt") as file:
+    for line in file:
+        names.append(line.rstrip())
+
+for name in sorted(names, reverse=True):
+    print(f"hello, {name}")
