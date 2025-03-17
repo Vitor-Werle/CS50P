676a93ced737838fcbdeff8db32ae12439c576bf
Vitor <vitorwerle78@gmail.com>
2025-03-16 23:12:26 -0300

    lecture 8

--- Week_8/learning.py
+++ Week_8/learning.py
@@ -1,15 +1,24 @@
 class Student:
-    ...
+    def __init__(self, name, house):
+        if not name:
+            raise ValueError("Invalid Name")
+        if house not in ["Grinffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid House")
+        self.name = name
+        self.house = house
+
+    def __str__(self):
+        return f"{self.name} from {self.house}"
 
 def main():
     student = get_student()
-    print(f"{student.name} from {student.house}")
+    print(student)
 
 def get_student():
     name = input("Name: ")
     house = input("house: ")
-    student = Student(name, house)
-    return student
+    return Student(name, house)
+
 
 if __name__ == "__main__":
     main()
