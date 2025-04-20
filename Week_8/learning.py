609772852e4efb2470719ad1c7cc88885cf26f1a
Vitor <vitorwerle78@gmail.com>
2025-04-20 14:40:27 -0300

    ai

--- Week_8/learning.py
+++ Week_8/learning.py
@@ -1,10 +1,17 @@
-class Food:
-    def __init__(self, ingredient):
-        self.ingredient = ingredient
+class Dog:
+    def __init__(self, name, breed, owner):
+        self.name = name
+        self.breed = breed
+        self.owner = owner
+    
+    def bark(self):
+        print("Auuuuuuuu")
 
+class Owner():
+    def __init__(self, name, age, contact):
+        self.name = name
+        self.age = age
+        self.phone = contact
 
-def main():
-    ...
-
-
-main()
+owner = Owner(input("Digite o nome do dono: "), int(input("Digite a idade do dono: ")), input("Digite o telefone do dono: "))
+print(f"O dono é {owner.name}")
